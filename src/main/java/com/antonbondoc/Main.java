55417d8d2293f58874b0008328e8fc923251a50e
Anton Bondoc <antonbondoc14@gmail.com>
2024-12-13 23:01:34 +0800

    Refactor generation of pixel art image

--- src/main/java/com/antonbondoc/Main.java
+++ src/main/java/com/antonbondoc/Main.java
@@ -14,34 +14,46 @@ public class Main {
 
         // Transform the image to pixel art
         int blockSize = 20; // Adjusting the pixel size is going to make it more blurry
-        BufferedImage pixelatedImage = pixelateUsingScaling(image, blockSize);
+        BufferedImage pixelatedImage = pixelateImage(image, blockSize);
 
-        // Store he output to a separate directory
-        File outputFileScaling = new File("output/pixel/image-output.jpg");
-        ImageIO.write(pixelatedImage, "jpg", outputFileScaling);
+        // Scale pixelated image to original size
+        BufferedImage outputImage = scaleUpImage(image.getWidth(), image.getHeight(), pixelatedImage);
 
-        // TODO: From the pixelated image generate the Excel file. How do we format it?
+        // Generate Excel pixel art
+        generateExcelPixelArt(pixelatedImage);
+
+        // Store the output image to a separate directory
+        File outputFile = new File("output/pixel/image-output.jpg");
+        ImageIO.write(outputImage, "jpg", outputFile);
     }
 
-    public static BufferedImage pixelateUsingScaling(BufferedImage inputImage, int blockSize) {
-        int width = inputImage.getWidth();
-        int height = inputImage.getHeight();
+    public static BufferedImage pixelateImage(BufferedImage originalImage, int blockSize) {
+        int scaledWidth = Math.max(1, originalImage.getWidth() / blockSize);
+        int scaledHeight = Math.max(1, originalImage.getWidth() / blockSize);
 
-        // Calculate scaled dimensions
-        int scaledWidth = Math.max(1, width / blockSize);
-        int scaledHeight = Math.max(1, height / blockSize);
+        // Scale down original image
+        Image tempImage = originalImage.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_AREA_AVERAGING);
 
-        // Create a scaled-down version
-        Image tempImage = inputImage.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_AREA_AVERAGING);
-
-        // Create a BufferedImage to scale it back up
-        BufferedImage pixelatedImage = new BufferedImage(width, height, inputImage.getType());
+        // Convert image to buffered image to access RGB values of each pixel
+        BufferedImage pixelatedImage = new BufferedImage(scaledWidth, scaledHeight, originalImage.getType());
         Graphics2D g2d = pixelatedImage.createGraphics();
-
-        // Scale back up
-        g2d.drawImage(tempImage, 0, 0, width, height, null);
+        g2d.drawImage(tempImage, 0, 0, null);
         g2d.dispose();
 
         return pixelatedImage;
+    }
+
+    private static BufferedImage scaleUpImage(int width, int height, BufferedImage image) {
+        BufferedImage scaledImage = new BufferedImage(width, height, image.getType());
+
+        Graphics2D g2d = scaledImage.createGraphics();
+        g2d.drawImage(image, 0, 0, width, height, null);
+        g2d.dispose();
+
+        return scaledImage;
+    }
+
+    private static void generateExcelPixelArt(BufferedImage pixelatedImage) {
+        // No-op
     }
 }
